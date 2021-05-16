379d35f5b720e319f228e587da5870384994ebc7
pauloricardolongi <pauloricardolong@gmail.com>
2021-05-16 00:26:17 -0300

    segunda versao

--- src/entities/Worker.java
+++ src/entities/Worker.java
@@ -1,10 +1,29 @@
 package entities;
 
+import java.util.List;
+
 import entities.enums.WorkerLevel;
 
 public class Worker {
 	private String name;
 	private WorkerLevel level;
 	private Double baseSalary;
+	
+	private Departament department;
+	//lista pois são vários contratos
+	private List<HourContract> contracts;
+	
+	public Worker() {
+		
+	}
+	//não marcar atributos que sejam da lista, ex.contracts
+
+	public Worker(String name, WorkerLevel level, Double baseSalary, Departament department) {
+	
+		this.name = name;
+		this.level = level;
+		this.baseSalary = baseSalary;
+		this.department = department;
+	}
 
 }
